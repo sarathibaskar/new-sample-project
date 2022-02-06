8219f506b184a860979ae668f4d58966a5cf20e5
sarathibaskar <sarathivaitheeshvari18@gmail.com>
2022-02-06 15:12:31 +0530

    firstproject

---
+++ src/org/sampleproject/Sample.java
@@ -0,0 +1,14 @@
+package org.sampleproject;
+
+import java.lang.reflect.Method;
+
+public class Sample {
+	private void method1() {
+System.out.println("sarathi");
+	}
+	public static void main(String[] args) {
+		Sample a= new Sample(); 
+		a.method1();
+	}
+
+}
